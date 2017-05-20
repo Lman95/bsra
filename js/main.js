0187fc9999f0c964d78926d80a2a368073b9211a
Landon Young <young518@uw.edu>
2017-05-20 00:29:30 -0700

    Front end for signin and signup basically done

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var config = {
+            apiKey: "AIzaSyATficQkkUvk4tmohfcEOGxvMBF9l5ggfM",
+            authDomain: "rides-app-f2b82.firebaseapp.com",
+            databaseURL: "https://rides-app-f2b82.firebaseio.com",
+            projectId: "rides-app-f2b82",
+            storageBucket: "rides-app-f2b82.appspot.com",
+            messagingSenderId: "1099379594226"
+        };
+
+        // Initialize Firebase, providing you read/write access to the database
+        firebase.initializeApp(config);
